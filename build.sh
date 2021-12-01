02c909ad6a885b16a1cea924fc38177392d5dd6b
Paul Wood <pmwood@pm.me>
2021-12-01 08:30:52 -0500

    Added testing section and build script

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/zsh
 
 run_continuously=$1
-make_cmd="latexmk -pdf -f"
+make_cmd="latexmk -pdf -f -silent"
 
 if [[ -z $run_continuously ]]; then
   eval $make_cmd
 else
-  find . -type f -name '*.bib' -o -name '*.tex' | entr -snc "eval $make_cmd" &
+  find . -type f -name '*.bib' -o -name '*.tex' | nice -n 10 entr -snc "eval $make_cmd"
 fi
 
 
